ec934192133240631ece7b3869f69e1d9ae0177b
Thanmayee05 <vthanmayee99@gmail.com>
2020-04-22 17:46:22 +0530

    Updates

---
+++ precache-manifest.71570a5b4e52d10813807dedd39102c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707f9cff6af764e23ea038832232dad1",
+    "url": "/reactMP/index.html"
+  },
+  {
+    "revision": "5f6393a8e398d54fbdf2",
+    "url": "/reactMP/static/css/main.6869756d.chunk.css"
+  },
+  {
+    "revision": "5d36e9116cff8690a725",
+    "url": "/reactMP/static/js/2.a70851cd.chunk.js"
+  },
+  {
+    "revision": "be0e8717d3904bad4edbab023f3e33dc",
+    "url": "/reactMP/static/js/2.a70851cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6393a8e398d54fbdf2",
+    "url": "/reactMP/static/js/main.8cb9a05b.chunk.js"
+  },
+  {
+    "revision": "2e2a08899c4a202a4518",
+    "url": "/reactMP/static/js/runtime-main.bdf3e88f.js"
+  },
+  {
+    "revision": "99c2b73da04b3e3a8d16aa141df68e2d",
+    "url": "/reactMP/static/media/pinkwomensilhouettewomensday_386782022.99c2b73d.png"
+  },
+  {
+    "revision": "1539441830ba17297ad5cb9b1c6161bf",
+    "url": "/reactMP/static/media/women_empowered.15394418.jpg"
+  }
+]);
