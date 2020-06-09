96ad5d11bc0dfe6c8d98e008eed6dfc2a762a33d
shraddha <shraddhapatalay11@gmail.com>
2020-06-09 20:01:32 +0530

    First Commit in newBranch

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,31 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 //import {showMarkerinLoc} from './Home';
 //import { Zoom } from '@material-ui/core';
 
-class SimpleMap extends Component{
-  static defaultProps={
-    center: {lat:17.387140,lng:78.491684},
-    zoom: 15
-  }
-    render(){
-      return (
-        <div style={{ height: '600px', width: '900px' , marginTop:'20px'}}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyDprftdVU4M9RKlH31yZqrPNO5Rj-Y6AK' }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          >
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: { lat: 17.38714, lng: 78.491684 },
+    zoom: 15,
+  };
+  render() {
+    return (
+      <div style={{ height: '600px', width: '900px', marginTop: '20px' }}>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: 'AIzaSyDprftdVU4M9RKlH31yZqrPNO5Rj-Y6AK' }}
+          bootstrapURLKeys={{ key: 'AIzaSyDprftdVU4M9RKlH31yZqrPNO5Rj-Y6AKg' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
           <Marker
             lat={this.props.center.lat}
             lng={this.props.center.lng}
-            name="My Marker"
-            color="rgb(207, 40, 40)"
+            name='My Marker'
+            color='rgb(207, 40, 40)'
           />
         </GoogleMapReact>
       </div>
-        );
-      }
+    );
   }
+}
 export default SimpleMap;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,41 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import fire from '../config/Fire';
-import { Redirect} from 'react-router-dom';
-
+import { Redirect } from 'react-router-dom';
 
 class Profile extends Component {
-  constructor(props){
-      super(props);
-      this.state = {
-          uname:'',
-          phoneno:null,
-          email:'',
-          city:'',
-          formTitle: 'UserProfile',
-          userDetails:[],
-          //msg:''
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      uname: '',
+      phoneno: null,
+      email: '',
+      city: '',
+      formTitle: 'UserProfile',
+      userDetails: [],
+      //msg:''
+    };
   }
-  handleChange=e=>{
+  handleChange = e => {
     this.setState({
-      [e.target.name] : e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
   getUserData = () => {
-    let ref = fire.database().ref("/");
-    ref.on("value", snapshot => {
+    let ref = fire.database().ref('/');
+    ref.on('value', snapshot => {
       const state = snapshot.val();
       this.setState(state);
     });
   };
-  componentDidUpdate()
-  {
+  componentDidUpdate() {
     //if (prevState !== this.state) {
-      this.writeUserData();
+    this.writeUserData();
     //}
   }
-  componentDidMount()
-  {
+  componentDidMount() {
     this.getUserData();
   }
   //user = uid => this.db.ref(`users/${uid}`);
@@ -56,44 +53,44 @@ class Profile extends Component {
       userDetails
     });
   }*/
-  
+
   writeUserData = () => {
-    fire.database().ref("/").set(this.state);
-    console.log("DATA SAVED");
+    fire.database().ref('/').set(this.state);
+    console.log('DATA SAVED');
   };
-  handleSub = e =>{
+  handleSub = e => {
     e.preventDefault();
     const uname = this.state.uname;
     const phoneno = this.state.phoneno;
-    const email=this.state.email;
-    const city=this.state.city;
+    const email = this.state.email;
+    const city = this.state.city;
     const uid = this.state.uid;
-    if (uid && uname && phoneno && email && city){
+    if (uid && uname && phoneno && email && city) {
       const { users } = this.state;
       const devIndex = users.findIndex(data => {
-        return data.uid === uid 
+        return data.uid === uid;
       });
       users[devIndex].uname = uname;
       users[devIndex].phoneno = phoneno;
       users[devIndex].email = email;
       users[devIndex].city = city;
       this.setState({ users });
-    }
-    else if (uname && phoneno && email && city) {
+    } else if (uname && phoneno && email && city) {
       //const uid = new Date().getTime().toString();
       const { users } = this.state;
-      users.push({ uname, phoneno, email, city })
+      console.log(users);
+      users.push({ uname, phoneno, email, city });
       this.setState({ users });
     }
     this.setState({
-      uname:'',
-      phoneno:null,
-      email:'',
-      city:''
-    })
-  }
-  
-    /*let messageRef = fire.database().ref('profile').orderByKey().limitToLast(100);
+      uname: '',
+      phoneno: null,
+      email: '',
+      city: '',
+    });
+  };
+
+  /*let messageRef = fire.database().ref('profile').orderByKey().limitToLast(100);
     fire.database().ref('profile').push(this.state.text);
     if(this.state.phoneno===null)
     {
@@ -110,56 +107,66 @@ class Profile extends Component {
       this.setState({msg:"Profile updated"});
     }
   }*/
-  render()
-  {
-    if(this.state.msg==="Profile updated")
-    {
-        return <Redirect to='/mapsPage'/>;
+  render() {
+    if (this.state.msg === 'Profile updated') {
+      return <Redirect to='/mapsPage' />;
     }
     //const { users } = this.state;
-    return(
-      <div className="bgimg">
-          <div className="form_block" >
-              <div id="title">{this.state.formTitle}</div>
-                <div className="body">
-                  <form>
-                      User Name<br/>
-                      <input type="text" 
-                      value={this.state.uname} 
-                      placeholder="UserName"
-                      onChange={this.handleChange} 
-                      required
-                      name="uname" />
-                      Phone Number<br/>
-                      <input type="text" 
-                      required
-                      inputmode="numeric"
-                      value={this.state.phoneno} 
-                      placeholder="PhoneNumber"
-                      onChange={this.handleChange} 
-                      name="phoneno" />
-                      Email ID<br/>
-                      <input type="text" 
-                      value={this.state.email} 
-                      placeholder="EmailID"
-                      required
-                      onChange={this.handleChange} 
-                      name="email" />
-                      City<br/>
-                      <input type="text" 
-                      value={this.state.city} 
-                      onChange={this.handleChange}
-                      placeholder="CityName" 
-                      required
-                      name="city" /><br/><br/>
-                      <button onClick={this.handleSub}>
-                        SUBMIT
-                      </button>
-                  </form>
-              </div>
+    return (
+      <div className='bgimg'>
+        <div className='form_block'>
+          <div id='title'>{this.state.formTitle}</div>
+          <div className='body'>
+            <form>
+              User Name
+              <br />
+              <input
+                type='text'
+                value={this.state.uname}
+                placeholder='UserName'
+                onChange={this.handleChange}
+                required
+                name='uname'
+              />
+              Phone Number
+              <br />
+              <input
+                type='text'
+                required
+                inputmode='numeric'
+                value={this.state.phoneno}
+                placeholder='PhoneNumber'
+                onChange={this.handleChange}
+                name='phoneno'
+              />
+              Email ID
+              <br />
+              <input
+                type='text'
+                value={this.state.email}
+                placeholder='EmailID'
+                required
+                onChange={this.handleChange}
+                name='email'
+              />
+              City
+              <br />
+              <input
+                type='text'
+                value={this.state.city}
+                onChange={this.handleChange}
+                placeholder='CityName'
+                required
+                name='city'
+              />
+              <br />
+              <br />
+              <button onClick={this.handleSub}>SUBMIT</button>
+            </form>
           </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default Profile;
