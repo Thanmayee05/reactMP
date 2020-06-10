2aa064301f7f4ef4f9104dfee5b402663f5efe9c
shraddha <shraddhapatalay11@gmail.com>
2020-06-10 17:30:10 +0530

    minor fixes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,46 +1,55 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import '../App.css';
-import {BrowserRouter} from 'react-router-dom';
-import SimpleImageSlider from "react-simple-image-slider";
-import './landing.css';  
-import gPic from '../gandhisays.jpg';
-import rPic from '../road.jpg';
-import styled, {keyframes} from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import SimpleImageSlider from 'react-simple-image-slider';
+import './landing.css';
+import gPic from '../images/gandhisays.jpg';
+import rPic from '../images/road.jpg';
+import styled, { keyframes } from 'styled-components';
 import { bounce } from 'react-animations';
+
 class LandingPage extends Component {
-    render() {
-        const images = [
-            { url: gPic },
-            { url: rPic },
-            { url: gPic },
-            { url: gPic },
-        ];
-        const Bounce=styled.div `animation: 3s ${keyframes`${bounce}`} infinite`;
-        return (
-            <BrowserRouter>
-                <div>
-                    <div className="bgimg">
-                        <div style={{minHeight:"110vh",marginTop:"0px",textAlign:"center",fontSize:"30px"}}>
-                            <Bounce>
-                                Welcome To She Help Home Page
-                            </Bounce>
-                            <div className="imgSlide" style={{marginTop:"30px", textAlign:"center",alignItems:"center"}}> 
-                            <div className="carousel__wrap">
-                                <SimpleImageSlider
-                                    width={1000}
-                                    height={504}
-                                    images={images}
-                                    slideDuration={1}
-                                    navStyle={1}
-                                />
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    const images = [{ url: gPic }, { url: rPic }, { url: gPic }, { url: gPic }];
+    const Bounce = styled.div`
+      animation: 3s ${keyframes`${bounce}`} infinite;
+    `;
+    return (
+      <BrowserRouter>
+        <div>
+          <div className='bgimg'>
+            <div
+              style={{
+                minHeight: '110vh',
+                marginTop: '0px',
+                textAlign: 'center',
+                fontSize: '30px',
+              }}
+            >
+              <Bounce>Welcome To She Help Home Page</Bounce>
+              <div
+                className='imgSlide'
+                style={{
+                  marginTop: '30px',
+                  textAlign: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <div className='carousel__wrap'>
+                  <SimpleImageSlider
+                    width={1000}
+                    height={504}
+                    images={images}
+                    slideDuration={1}
+                    navStyle={1}
+                  />
                 </div>
-            </BrowserRouter>
-            
-        );
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 export default LandingPage;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,111 +1,139 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import fire from '../config/Fire';
-import { Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 class Login extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            name:'',
-            fireErrors: '',
-            formTitle: 'Login',
-            loginBtn: true,
-            user:null,
-            msg:'',
-            uid:null
-        }
-    }
-    componentDidMount(){
-        this.authListener();
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      fireErrors: '',
+      formTitle: 'Login',
+      loginBtn: true,
+      user: null,
+      msg: '',
+      uid: null,
+    };
+  }
+  componentDidMount() {
+    this.authListener();
+  }
+  authListener() {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
       }
-    authListener(){
-        fire.auth().onAuthStateChanged((user) => {
-          if(user){
-            this.setState({user});
-          }else{
-            this.setState({user:null});
-          }
-        });
-      }
-    login = e => {
-        e.preventDefault();
-        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then(()=>{
-            this.setState({msg:"Login success"});
-            const newId = fire.auth().currentUser.uid;
-            console.log(newId);
-        })
-        .catch((error) => {
-            this.setState({fireErrors: error.message})
-        });     
+    });
+  }
+  login = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.setState({ msg: 'Login success' });
+        const newId = fire.auth().currentUser.uid;
+        console.log(newId);
+      })
+      .catch(error => {
+        this.setState({ fireErrors: error.message });
+      });
+  };
+  register = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.setState({ msg: 'Registration success' });
+        const newId = fire.auth().currentUser.uid;
+        console.log(newId);
+      })
+      .catch(error => {
+        this.setState({ fireErrors: error.message });
+      });
+  };
+
+  getAction = action => {
+    if (action === 'reg') {
+      this.setState({ formTitle: 'Register', loginBtn: false, fireErrors: '' });
+    } else {
+      this.setState({ formTitle: 'Login', loginBtn: true, fireErrors: '' });
     }
-    register = e => {
-        e.preventDefault();
-        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-        .then(()=>{
-            this.setState({msg:"Registration success"}); 
-            const newId = fire.auth().currentUser.uid;
-            console.log(newId);
-        })
-        .catch((error) => {
-            this.setState({fireErrors: error.message})
-        });
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    let errorNotification = this.state.fireErrors ? (
+      <div className='Error'> {this.state.fireErrors} </div>
+    ) : null;
+    let submitBtn = this.state.loginBtn ? (
+      <input
+        className='loginBtn'
+        type='submit'
+        onClick={this.login}
+        value='Submit'
+      />
+    ) : (
+      <input
+        className='loginBtn'
+        type='submit'
+        onClick={this.register}
+        value='Submit'
+      />
+    );
+    let login_register = this.state.loginBtn ? (
+      <button className='registerBtn' onClick={() => this.getAction('reg')}>
+        New User? Register
+      </button>
+    ) : (
+      <button className='registerBtn' onClick={() => this.getAction('login')}>
+        Existing user? Login
+      </button>
+    );
+    if (this.state.msg === 'Login success') {
+      return <Redirect to='/mapsPage' />;
     }
-    
-    getAction = action => {
-        if(action === 'reg'){
-            this.setState({formTitle: 'Register', loginBtn: false, fireErrors: ''});
-        }else{
-            this.setState({formTitle: 'Login', loginBtn: true, fireErrors: ''});
-        }
+    if (this.state.msg === 'Registration success') {
+      return <Redirect to='/profile' />;
     }
-    handleChange = e => {
-        this.setState({[e.target.name]: e.target.value});
-    }
-    render(){
-        let errorNotification = this.state.fireErrors ? 
-            ( <div className="Error"> {this.state.fireErrors} </div> ) : null;
-        let submitBtn = this.state.loginBtn ? 
-            (<input className="loginBtn" type="submit" onClick={this.login} value="Submit" />) : 
-            (<input className="loginBtn" type="submit" onClick={this.register} value="Submit" />);
-        let login_register = this.state.loginBtn ?
-            (<button className="registerBtn" onClick={() => this.getAction('reg')}>New User? Register</button>) : 
-            (<button className="registerBtn" onClick={() => this.getAction('login')}>Existing user? Login</button>)
-        if(this.state.msg==="Login success")
-        {
-            return <Redirect to='/mapsPage'/>;
-        }
-        if(this.state.msg==="Registration success")
-        {
-            return <Redirect to='/profile'/>;
-        }
-        
-        return(
-            <div className="bgimg">
-                <div className="form_block" >
-                    <div id="title">{this.state.formTitle}</div>
-                    <div className="body">
-                        {errorNotification}
-                        <form>
-                            Email ID<br/>
-                            <input type="text" 
-                            value={this.state.email} 
-                            placeholder="EmailID"
-                            onChange={this.handleChange} 
-                            name="email" />
-                            Password<br/>
-                            <input type="password" 
-                            value={this.state.password} 
-                            onChange={this.handleChange}
-                            placeholder="Password" 
-                            name="password" />
-                            {submitBtn}
-                        </form>
-                        {login_register}
-                    </div>
-                </div>
-            </div>
-        )}
-    }
-export default (Login);
+
+    return (
+      <div className='bgimg'>
+        <div className='form_block'>
+          <div id='title'>{this.state.formTitle}</div>
+          <div className='body'>
+            {errorNotification}
+            <form>
+              Email ID
+              <br />
+              <input
+                type='text'
+                value={this.state.email}
+                placeholder='EmailID'
+                onChange={this.handleChange}
+                name='email'
+              />
+              Password
+              <br />
+              <input
+                type='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+                placeholder='Password'
+                name='password'
+              />
+              {submitBtn}
+            </form>
+            {login_register}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Login;
